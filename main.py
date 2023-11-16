54599da303cf4ab992ad41ac72ab9ec5f8648701
LuisAGuzmanI <luisangel2002.lagi@gmail.com>
2023-11-16 17:36:27 -0600

    Configuración de CORS de dominio de producción

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:5173",  # Add the URL of your Vue.js application
+    "http://localhost:5173",  # Development enviorment
+    "http://localhost:4173",  # Build preview enviorment
+    "https://main.d2qq7id2kqiuhj.amplifyapp.com",  # Production delopyment
 ]
 
 app.add_middleware(
